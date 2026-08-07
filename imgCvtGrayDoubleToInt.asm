e39b70ce801c5edea5cc9c0d55db68d370826882
HoursForever <93859488+HoursForever@users.noreply.github.com>
2026-08-07 11:03:22 +0800

    Refactor imgCvtGrayDoubleToInt assembly code

--- imgCvtGrayDoubleToInt.asm
+++ imgCvtGrayDoubleToInt.asm
@@ -2,17 +2,10 @@ default rel
 
 section .data
     align 16
-    ; 2 packed double precision constants [255.0, 255.0] (128 bits / 16 bytes)
     scale dq 255.0, 255.0
 
 section .text
 global imgCvtGrayDoubleToInt
-
-imgCvtGrayDoubleToInt:
-    ; RCX = height
-    ; RDX = width
-    ; R8  = input pointer  (const double*)
-    ; R9  = output pointer (int*)
 
     mov     rax, rcx
     imul    rax, rdx            
@@ -42,7 +35,6 @@ imgCvtGrayDoubleToInt:
     jz      .done
 
 .tail_handle:
-    ; Handle leftover pixel using PACKED operations to strictly avoid scalar SIMD
     sub     rsp, 16             
     mov     rdx, [r8]
     mov     [rsp], rdx          
